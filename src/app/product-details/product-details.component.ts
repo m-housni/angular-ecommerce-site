17a32b3183d7f837e497a920f21fdbf0ce94932a
m-housni <mohamed.housni@um5s.net.ma>
2021-10-04 21:18:20 +0100

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -16,6 +16,14 @@ export class ProductDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+  this.product = products.find(product => product.id === productIdFromRoute);
+
+  alert(this.product.name);
   }
 
 }
