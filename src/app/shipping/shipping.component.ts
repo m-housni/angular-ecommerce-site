e19340851b55a971341d15395bdb33d4a46f223e
m-housni <mohamed.housni@um5s.net.ma>
2021-10-05 12:37:47 +0100

    1 file modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -8,7 +8,9 @@ import { CartService } from '../cart.service';
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
   }
