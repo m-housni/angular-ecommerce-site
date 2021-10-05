c5015cbade47b285d5999c5e799496b6f28e2cc1
m-housni <mohamed.housni@um5s.net.ma>
2021-10-05 11:38:18 +0100

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -19,6 +19,9 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   /* . . . */
   constructor() {}
 
